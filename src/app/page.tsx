1bcd1c1849970ccee40a666f648a6ed794b54189
JulianOviedo <julianovie234@gmail.com>
2023-11-15 10:22:52 -0300

    log in UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
+import { LockIcon, Logo, MailBox } from "../../public/Icons";
+
 export default function Home() {
   return (
-    <h1 className=''>hello</h1>
-      )
+    <main className="w-screen h-screen p-4 bg-5">
+      <div className="bg-3 h-full w-full rounded-md p-4">
+        <div className="w-full flex justify-center items-center p-4 flex-col">
+          <Logo />
+          <h1 className="text-4xl font-bold mt-8">Welcome !</h1>
+        </div>
+        <div className="flex justify-center items-center flex-col mt-8 gap-4">
+          <h4>You can log in here</h4>
+          <form action="" className="flex gap-5 flex-col w-full items-center">
+            <label htmlFor="" className="relative w-full">
+              <div className="absolute top-2 left-2">
+                <MailBox />
+              </div>
+              <input placeholder="mailbox@gmail.com" type="email" className=" rounded h-10 pl-10 bg-5 w-full" />
+            </label>
+            <label htmlFor="" className="relative w-full">
+              <div className="absolute top-2 left-2">
+                <LockIcon />
+              </div>
+              <input placeholder="Password" type="password" className=" rounded h-10 pl-10 bg-5 w-full" />
+            </label>
+            <a href="#" className="text-anchor ">Did you forget your password ?</a>
+            <button className="bg-1 rounded text-5 w-full h-10">Log in</button>
+          </form>
+          <hr className="w-full mt-2"/>
+          <a href="#" className="text-anchor ">First time here ?</a>
+          <button className="w-full bg-5 h-10 rounded">Create an account</button>
+        </div>
+      </div>
+    </main>
+  );
 }
