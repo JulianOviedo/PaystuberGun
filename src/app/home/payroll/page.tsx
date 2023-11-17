cf116f0aa9118af17b0aec34a7f951be88d8de99
JulianOviedo <julianovie234@gmail.com>
2023-11-17 16:29:55 -0300

    add search bar ui

--- src/app/home/payroll/page.tsx
+++ src/app/home/payroll/page.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '../../../../public/Icons'
 import { EmployeeCard } from '../../components/EmployeeCard'
 
 export default function PayrollPage () {
@@ -13,8 +14,13 @@ export default function PayrollPage () {
 
   return (
     <main className='flex flex-col items-center gap-6 bg-5 h-screen px-4'>
-      <h1 className='text-1 mt-10'>Payroll</h1>
-      <p className='text-1'>List of employees</p>
+      <h1 className='text-1 mt-10'>Payroll - List of employees</h1>
+      <div className='relative'>
+        <div className='absolute w-5 h-5 top-2 ml-2'>
+          <SearchIcon />
+        </div>
+        <input type='search' className='w-64 rounded p-2 pl-8 bg-3' placeholder='SEARCH BAR' />
+      </div>
       <hr className='h-1 w-full' />
       {mockEmployees.map(employee => {
         const { name, lastName, dni, added, mail } = employee
