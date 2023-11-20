be738a1bbfd0ab88cbc94b26341701ccef5bacce
JulianOviedo <julianovie234@gmail.com>
2023-11-19 23:02:46 -0300

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {},
     colors: {
-      '1': '#6B9080',
-      '2': '#A4C3B2',
-      '3': '#CCE3DE',
-      '4': '#EAF4F4',
-      '5': '#F6FFF8', //fondos
-      'anchor': '#0096FF'
+      1: '#6B9080',
+      2: '#A4C3B2',
+      3: '#CCE3DE',
+      4: '#EAF4F4',
+      5: '#F6FFF8', // fondos
+      anchor: '#0096FF',
+      dark: '#000000'
     }
   },
-  plugins: [],
+  plugins: []
 }
