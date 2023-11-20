338b34f1baf53bee13aa5f8b3cf8b5e260f971f0
JulianOviedo <julianovie234@gmail.com>
2023-11-19 23:03:00 -0300

    modal UI to edit employee

--- src/app/components/EmployeeCard.tsx
+++ src/app/components/EmployeeCard.tsx
@@ -8,19 +8,19 @@ interface EmployeeCardProps {
   added: Date
   dni: number
   mail: string
+  handleEdit: () => void
 }
 
-export const EmployeeCard = ({ name, lastName, added, dni, mail }: EmployeeCardProps) => {
-  const formattedDate = added.toLocaleDateString()
+export const EmployeeCard = ({ name, lastName, added, dni, mail, handleEdit }: EmployeeCardProps) => {
   return (
     <div className=' text-center flex flex-row gap-2 '>
       <section className='bg-1 rounded p-4'>
         <h1 className='font-bold underline'>{lastName}, {name} - {dni}</h1>
-        <p>Added: {formattedDate}</p>
+        <p>Added: add date</p>
         <p>Mail: {mail}</p>
       </section>
       <section className='flex flex-col gap-2 items-center justify-center'>
-        <div className='ml-1 w-8 h-8'>
+        <div className='ml-1 w-8 h-8' onClick={() => handleEdit()}>
           <EditIcon />
         </div>
         <div className='w-8 h-8'>
--- src/app/home/payroll/page.tsx
+++ src/app/home/payroll/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+import { useState } from 'react'
 import { SearchIcon } from '../../../../public/Icons'
 import { EmployeeCard } from '../../components/EmployeeCard'
 
 export default function PayrollPage () {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
   const mockEmployees = [
     {
       name: 'Julian',
@@ -12,8 +16,34 @@ export default function PayrollPage () {
     }
   ]
 
+  const handleEdit = () => {
+    console.log(isModalOpen)
+    setIsModalOpen(true)
+  }
+
+  const CloseModal = () => {
+    setIsModalOpen(false)
+  }
+
   return (
-    <main className='flex flex-col items-center gap-6 bg-5 h-screen px-4'>
+    <main className='flex flex-col items-center gap-6 bg-5 h-screen px-4 relative z-0'>
+      {isModalOpen &&
+        <div className='z-10 absolute w-full h-full bg-black bg-opacity-90 top-[50%] left-[50%] transform translate-x-[-50%] translate-y-[-50%] bg-dark'>
+          <div className='w-screen h-screen flex justify-center items-center'>
+            <form action='' className='flex flex-col gap-8 w-80 h-50 bg-1 rounded-xl items-start p-8 justify-center'>
+              <label htmlFor=''>
+                Name -  <input className='rounded-sm pl-2' type='text' />
+              </label>
+              <label htmlFor=''>
+                Dni -   <input className='rounded-sm pl-2' type='text' />
+              </label>
+              <label htmlFor=''>
+                Mail -   <input className='rounded-sm pl-2' type='text' />
+              </label>
+              <button className='w-72 bg-3 self-center rounded-lg h-10' onClick={CloseModal}>Save</button>
+            </form>
+          </div>
+        </div>}
       <h1 className='text-1 mt-10'>Payroll - List of employees</h1>
       <div className='relative'>
         <div className='absolute w-5 h-5 top-2 ml-2'>
@@ -26,7 +56,7 @@ export default function PayrollPage () {
         const { name, lastName, dni, added, mail } = employee
         return (
           <div key={dni}>
-            <EmployeeCard name={name} lastName={lastName} dni={dni} added={added} mail={mail} />
+            <EmployeeCard name={name} lastName={lastName} dni={dni} added={added} mail={mail} handleEdit={handleEdit} />
           </div>
         )
       })}
