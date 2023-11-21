056214f67f3e58115498bd697fc6cf2a960a7a67
JulianOviedo <julianovie234@gmail.com>
2023-11-21 10:55:18 -0300

    responsive login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { LockIcon, Logo, MailBox } from '../../public/Icons'
 export default function SignIn () {
   return (
     <main className='w-screen h-screen p-4 bg-5'>
-      <div className='bg-3 h-full w-full rounded-md p-4'>
+      <div className='bg-3 h-full w-full rounded-md p-4 sm:px-[30%]'>
         <div className='w-full flex justify-center items-center p-4 flex-col'>
           <div className='w-[84px] h-[84px]'>
             <Logo />
