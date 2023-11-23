711bc096128a7516ddb1f8fe3ace3aaff6f577f2
JulianOviedo <julianovie234@gmail.com>
2023-11-23 10:31:20 -0300

    refact main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Logo } from '../../public/Icons'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +18,19 @@ export default function RootLayout ({
   return (
     <html lang='en'>
       <link rel='icon' href='/favicon.ico' sizes='any' />
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <main className='w-screen h-screen p-4 bg-5'>
+          <div className='bg-3 h-full w-full rounded-md p-4 sm:px-[30%]'>
+            <div className='w-full flex justify-center items-center p-4 flex-col'>
+              <div className='w-[84px] h-[84px]'>
+                <Logo />
+              </div>
+              {children}
+            </div>
+          </div>
+        </main>
+      </body>
     </html>
   )
 }
