d549597a642701b61ef5c7a6e58943fbc1e311ec
JulianOviedo <julianovie234@gmail.com>
2023-11-27 20:57:13 -0300

    add toast and query client provider to root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ReactQueryClient } from './components/ReactQueryProvider'
+import { ReactHotToast } from './components/ReactHotToast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +21,10 @@ export default function RootLayout ({
       <link rel='icon' href='/favicon.ico' sizes='any' />
 
       <body className={inter.className}>
-        {children}
+        <ReactQueryClient>
+          {children}
+        </ReactQueryClient>
+        <ReactHotToast />
       </body>
     </html>
   )
