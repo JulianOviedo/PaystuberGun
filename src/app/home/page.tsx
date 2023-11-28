68bf78502b337f8d365f028cddf814102423ff0d
JulianOviedo <julianovie234@gmail.com>
2023-11-28 12:53:15 -0300

    fix UI

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function HomePage () {
   return (
     <main className='w-screen h-screen bg-5'>
-      <section className='flex flex-col justify-center items-center gap-16 p-10'>
-        <Link className='bg-1  rounded w-full h-20 text-2xl flex items-center justify-center font-bold text-3 cursor-pointer' href='/home/payroll'>View payroll</Link>
-        <Link className='bg-1  rounded w-full h-20 text-2xl flex items-center justify-center font-bold text-3 cursor-pointer' href='#'>Shot Paystubs</Link>
+      <section className='flex flex-col justify-center items-center gap-16 p-10 sm:flex-row'>
+        <Link className='bg-1  rounded w-full sm:w-80 h-20 text-2xl flex items-center justify-center font-bold text-3 cursor-pointer' href='/home/payroll'>View payroll</Link>
+        <Link className='bg-1  rounded w-full sm:w-80 h-20 text-2xl flex items-center justify-center font-bold text-3 cursor-pointer' href='#'>Shot Paystubs</Link>
       </section>
     </main>
   )
