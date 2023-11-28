d57ca0f8efa29277be204c71a841f169caebdc48
JulianOviedo <julianovie234@gmail.com>
2023-11-28 12:29:38 -0300

    fix issue with env vars

--- src/app/services/createUser.tsx
+++ src/app/services/createUser.tsx
@@ -1,12 +1,9 @@
 import { newUserInfo } from '(@/types.td)'
 import axios from 'axios'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const apiURL = process.env.NEXT_PUBLIC_API_URL!
-
 export const createUser = async (data: newUserInfo) => {
   try {
-    const response = await axios.post(`${apiURL}create-user`, data)
+    const response = await axios.post('http://localhost:3000/api/create-user', data)
     return response.data
   } catch (error) {
     console.error('Error creating user:', error)
