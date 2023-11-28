a66c62729f1137721c33c705b5103b559195d247
JulianOviedo <julianovie234@gmail.com>
2023-11-28 12:00:54 -0300

    ref route endpoint

--- src/app/api/create-user/route.js
+++ src/app/api/create-user/route.js
@@ -1,21 +1,22 @@
 import { NextResponse } from 'next/server.js'
 import { pool } from '../../../libs/mysql.js'
-export async function GET (request) {
-  const result = await pool.query('SELECT NOW()')
-  const jsonResponse = JSON.stringify(result[0]['NOW()'])
-  return new Response(jsonResponse)
-}
 
 export async function POST (request) {
   try {
     const { username, email, password } = await request.json()
-
+    // eslint-disable-next-line no-unused-vars
     const result = await pool.query('INSERT INTO users SET ?', {
       username, email, password
     })
+    console.log('Received Data:', username, email, password)
 
-    console.log(result)
-    return NextResponse.json({ message: 'New user has been created!' }, { status: 200 })
+    return NextResponse.json({ message: 'New user has been created!' }, { status: 200 }, {
+      heeaders: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      }
+    })
   } catch (error) {
     return NextResponse.json({ message: error.message }, { status: 500 })
   }
