278f5875f5868e8f54968d6744be4776d9c967e2
JulianOviedo <julianovie234@gmail.com>
2023-11-29 12:29:03 -0300

    add employee interface

--- src/types.td.ts
+++ src/types.td.ts
@@ -3,3 +3,12 @@ export interface newUserInfo {
   password: string
   email: string
 }
+
+export interface Employee {
+  name: string
+  lastname: string
+  dni: number
+  added_at: string
+  user_id: number
+  email: string
+}
